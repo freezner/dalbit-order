785dc083e960bdb812cab22eb1c3edcb0cac61de
박형진 <hyungjin@terafunding.com>
2020-11-22 12:31:28 +0900

    update menu

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -39,10 +39,49 @@ export default class MenuItem extends Component {
 
         const lists = this.state.items.map((e, index) => {
             const detail = e.items.map((d, index) => {
+                var price = ""
+                var isHotText = ""
+                var isHotStyle = menuStyle.hotAndIced
+
+                if (d.price_min != undefined) {
+                    price = d.price_min.toLocaleString() + " ~ " + d.price.toLocaleString() + "원"
+                } else {
+                    price = d.price.toLocaleString() + "원"
+                }
+
+                
+
+                if (d.is_hot != undefined) {
+                    switch(d.is_hot) {
+                        case 1: 
+                            isHotText = "Hot Only";
+                            isHotStyle = menuStyle.onlyHot;
+                            break
+                        
+                        case 2: 
+                            isHotText = "Iced Only";
+                            isHotStyle = menuStyle.onlyIced;
+                            break
+                        
+                        case 3: 
+                            isHotText = "Hot / Iced";
+                            break
+                        
+                        default: 
+                            isHotText = ""
+                    }
+                } else {
+                    isHotText = ""
+                }
+
                 return (
                     <li key={index} data-show={d.isShow}>
-                        <span className={menuStyle.name}>{d.name}</span>
-                        <span className={menuStyle.price}>{d.price.toLocaleString()} 원</span>    
+                        <span className={menuStyle.name}>
+                            {d.name}
+                            <span className={isHotStyle}>{isHotText}</span>
+                        </span>
+                        
+                        <span className={menuStyle.price}>{price}</span>    
                     </li>
                 )
             })
